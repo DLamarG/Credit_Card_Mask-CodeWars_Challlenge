4473167830e379f4a65288fd7ba695c79b5b35df
DLamarG <demondg8@gmail.com>
2024-04-23 07:31:48 -0500

    solved python

---
+++ credit_card_mask.py
@@ -0,0 +1,18 @@
+def check_string(func):
+    def wrapper(cc):
+        if len(cc)==0:
+            return cc
+        if len(cc) <=4:
+            return cc
+        front = len(cc[0:len(cc)-4])
+        back = cc[len(cc)-4:]
+        return func(front*"#"+back)
+    return wrapper
+
+@check_string
+def maskify(cc):
+     return cc
+
+
+print(maskify('SF$SDfgsd2eA'))
+print(maskify('123'))
